11039d945ed2ddeda70dd15d6e5935dddbf38cca
MartijnPost <mar-t@live.nl>
2021-11-05 14:15:26 +0100

    Maurits commit

--- API/api_index.py
+++ API/api_index.py
@@ -2,14 +2,16 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 import json
 import datetime
+import pickle as pck
 
 app = Flask(__name__)
 api = Api(app)
 
+model_path = 'model.pickle'
 
 class HelloWorld(Resource):
     def get(self):
-        return "Martijn is baas van het internet"
+        return "Martijn is baas van de apen"
 
 class DataStream(Resource):
     def get(self):
@@ -19,9 +21,30 @@ class DataStream(Resource):
         timestamp = datetime.datetime.now()
         timestamp = timestamp.strftime("%Y-%m-%d-%H-%M-%S")
         data = request.form['data']
-        with open(f"../Data/{timestamp}.csv", "w") as f:
+
+        with open(f"Data/{timestamp}.csv", "w") as f:
             f.write(data)
         return "Succesfully added data"
+
+class Prediction(Resource):
+    def get(self):
+        return "Geef data"
+
+    def check_data(self,data):
+        d = json.loads(data)
+        if d.keys() != ['IR Voltage', 'Temperature','Time','Date']:
+            return False
+        return True
+
+    def post(self):
+        data = request.form['data']
+        if self.check_data(data):
+            model = pck.load(model_path)
+            if isinstance(data, list):
+                return model.predict(data)
+            return model.predict([data])
+        else:
+            print("Geef betere data noob")
 
 
 api.add_resource(HelloWorld, '/')
