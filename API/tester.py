884399651f3964ed9ea75408a83d8de5304785e7
MartijnPost <mar-t@live.nl>
2021-11-05 11:39:46 +0100

    mega string

--- API/tester.py
+++ API/tester.py
@@ -1,12 +1,6 @@
 from requests import put, get
-import csv
 
-file = []
 with open("D:\Downloads\YoungMavericksDownloads\data\clean_tac\BK7610_clean_TAC.csv", "r") as f:
-    reader = csv.reader(f, delimiter=',')
-    for row in reader:
-        file += row
-file = ','.join(file)
-print(file)
+    file = f.read()
 
-#result = put('http://127.0.0.1:5000/data', data={'data': file})
+result = put('http://127.0.0.1:5000/data', data={'data': file})
