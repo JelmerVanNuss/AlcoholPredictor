8ef6c1f79fd07f1fdce9feb47c2e9d33e3366f90
MartijnPost <mar-t@live.nl>
2021-11-05 10:57:02 +0100

    API Start

--- API/api_index.py
+++ API/api_index.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,8 +10,17 @@ class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+class DataStream(Resource):
+    def get(self):
+        return "Input data using post"
+    def post(self):
+        data = request.form['data']
+        with open("test.json", "w") as f:
+            json.dump(data, f)
+        return "Succesfully added data"
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(DataStream, '/data')
 
 if __name__ == '__main__':
     app.run(debug=True)
