25412ffed5ca92d4b40e220b92b2714dfaa0fa2d
Young Mavericks <youngmavericks@Youngs-MacBook-Air.local>
2021-11-05 16:09:28 +0100

    added mlflow stage for great expectations validation

--- run_all.sh
+++ run_all.sh
@@ -2,8 +2,8 @@ echo "Mergind data"
 ./merge_data.sh
 
 echo "Validating data"
-# great-expectations checkpoint run checkpoint_tac2
-# great-expectations checkpoint run checkpoint_tac2
+great-expectations checkpoint run accel_suite
+great-expectations checkpoint run checkpoint_tac2
 
 echo "Splitting into train/test split"
 python split_data.py
